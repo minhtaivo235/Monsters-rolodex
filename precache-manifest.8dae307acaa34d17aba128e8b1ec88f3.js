311e09351372561359d36d76671d1a68f0856fd8
minhtai <minhtaivo235@gmail.com>
2020-10-25 00:48:20 +0700

    Updates

---
+++ precache-manifest.8dae307acaa34d17aba128e8b1ec88f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dbd43631ece480197a0dd7c6db7eb8b",
+    "url": "/Monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ec56117b4e2d8dc2a453",
+    "url": "/Monsters-rolodex/static/css/main.90d837f2.chunk.css"
+  },
+  {
+    "revision": "56aae774d8f9eeb2cd6c",
+    "url": "/Monsters-rolodex/static/js/2.0a529bf7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Monsters-rolodex/static/js/2.0a529bf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec56117b4e2d8dc2a453",
+    "url": "/Monsters-rolodex/static/js/main.dc2e919e.chunk.js"
+  },
+  {
+    "revision": "ce236435b1a77080e948",
+    "url": "/Monsters-rolodex/static/js/runtime-main.9e6da6d0.js"
+  }
+]);
